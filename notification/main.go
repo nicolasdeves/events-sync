2e02d34ea5c404ede7c5bf17f5d7e39198994271
Nícolas Deves <nicolas.deves@universo.univates.br>
2025-12-04 22:35:21 -0300

    feat: add notification api

---
+++ notification/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-gomail/gomail"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("não achei .env, seguindo sem ele…")
+	}
+
+	r := gin.Default()
+
+	r.POST("/send-email", func(c *gin.Context) {
+
+		var body struct {
+			To      string `json:"to"`
+			Subject string `json:"subject"`
+			Message string `json:"message"`
+		}
+
+		if err := c.BindJSON(&body); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "json errado"})
+			return
+		}
+
+		host := os.Getenv("SMTP_HOST")
+		username := os.Getenv("SMTP_USER")
+		password := os.Getenv("SMTP_PASS")
+		port, _ := strconv.Atoi(os.Getenv("SMTP_PORT"))
+
+		msg := gomail.NewMessage()
+		msg.SetHeader("From", username)
+		msg.SetHeader("To", body.To)
+		msg.SetHeader("Subject", body.Subject)
+		msg.SetBody("text/plain", body.Message)
+
+		dialer := gomail.NewDialer(host, port, username, password)
+
+		if err := dialer.DialAndSend(msg); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "falhei no envio do email"})
+			return
+		}
+
+		c.JSON(200, gin.H{"status": "email enviado com sucesso!"})
+	})
+
+	r.Run(os.Getenv("PORT"))
+}
