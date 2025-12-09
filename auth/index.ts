982ddf14ceee2bc786abbf2ce0b3c7143836d410
Nícolas Deves <nicolas.deves@universo.univates.br>
2025-12-09 00:41:23 -0300

    feat: update CORS configuration to allow multiple origins

--- auth/index.ts
+++ auth/index.ts
@@ -14,7 +14,11 @@ const fastify =
   // { logger: true }
 
 fastify.register(cors, {
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://nicolas-deves.duckdns.org:5173",
+  ],
   credentials: true,
 });
 
